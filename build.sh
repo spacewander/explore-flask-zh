ae4ec2da938614437e6b7d732182b830806c26c6
spacewander <spacewanderlzx@gmail.com>
2015-08-08 11:59:30 +0800

    rewrite build.sh to add more automation

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-gitbook build ./zh --output='./bin' --config ./zh/book.json
+thisProject="explore-flask-zh"
+tmpDir=$(mktemp -d)
+gitbook build ./zh --output="$tmpDir" --config ./zh/book.json
+git checkout gh-pages
+cp -r .git "$tmpDir"
+cd .. && rm -rf "$thisProject" && mv "$tmpDir" "$thisProject"
+git commit -a -m "$(date)"
+git push origin gh-pages
