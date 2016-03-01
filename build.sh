1014b3e286a0ec5e3245e75a501f92743f02d0bc
spacewander <spacewanderlzx@gmail.com>
2016-03-01 17:09:35 +0800

    fix for gitbook v2

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-thisProject="explore-flask-zh"
-tmpDir=$(mktemp -d)
-gitbook build ./zh --output="$tmpDir" --config ./zh/book.json
+set -x
+tmpDir=$(mktemp -d -u)
+gitbook build ./zh --config ./zh/book.json
+mv ./zh/_book "$tmpDir"
 git checkout gh-pages
-cp -r .git "$tmpDir"
-cd .. && rm -rf "$thisProject" && mv "$tmpDir" "$thisProject"
-git commit -a -m "$(date)"
-git push origin gh-pages
+cp -r -t "$tmpDir" .git
+cd "$tmpDir" && git commit -a -m "$(date)" && git push origin gh-pages
