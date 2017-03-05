d5dc7a2e6f99014d664819fd8da39585779461a9
spacewander <spacewanderlzx@gmail.com>
2017-03-05 16:01:11 +0800

    build.sh:pull remote change before and checkout back after push

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 set -x
+curDir=$(pwd)
 tmpDir=$(mktemp -d -u)
 gitbook build ./zh --config ./zh/book.json
 mv ./zh/_book "$tmpDir"
 git checkout gh-pages
+git pull origin gh-pages
 cp -r -t "$tmpDir" .git
 cd "$tmpDir" && git commit -a -m "$(date)" && git push origin gh-pages
+cd "$curDir" && git checkout master
